8d1c48161a936400e9b93b868a03c25fcb5c62e1
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2024-04-15 15:55:56 -0300

    Login screen initial layout with colors

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   SafeAreaView,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -8,96 +9,134 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import Logo from '../../../assets/images/logo.svg';
+import GoogleIcon from '../../../assets/images/google.svg';
+import FacebookIcon from '../../../assets/images/facebook.svg';
 
 const LoginScreen = () => {
   return (
-    <SafeAreaView style={styles.screenContainer}>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.image} />
-      <Text style={styles.title}>Bienvenido a Petlify!</Text>
-      <Text style={styles.subtitle}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nemo. Qui
-        itaque rerum sed sint inventore! Nesciunt amet sunt pariatur. since.
-      </Text>
-
-      <View style={styles.inputsContainer}>
-        <TextInput style={styles.loginInput} placeholder="Email" />
-        <TextInput style={styles.loginInput} placeholder="Password" />
-        <TouchableOpacity>
-          <Text style={styles.forgotPasswordText}>Olvidé mi contraseña</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.spacer} />
-
-      <View style={styles.loginButtonsContainer}>
-        <TouchableOpacity style={styles.logInButton}>
-          <Text style={styles.loginButtonText}>Iniciar sesion</Text>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.screenContainer1}>
+      <ScrollView
+        contentContainerStyle={styles.contentContainerStyle}
+        style={styles.contentContainer}
+        bounces={false}>
+        <StatusBar barStyle="dark-content" />
+        <Logo height={150} width={150} style={styles.image} />
+        <Text style={styles.title}>Bienvenido a Petlify</Text>
+        <Text style={styles.subtitle}>¡Mejora la vida de tu mascota!</Text>
 
         <View style={styles.divisorContainer}>
           <View style={styles.divisorLine} />
-          <Text style={styles.divisorText}>o ingresa con</Text>
+          <Text style={styles.divisorText}>Iniciar sesión</Text>
           <View style={styles.divisorLine} />
         </View>
 
-        <View style={styles.socialButtonsContainer}>
-          <TouchableOpacity style={styles.socialButton}>
-            <Text style={styles.socialButtonText}>Google</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.socialButton}>
-            <Text style={styles.socialButtonText}>Facebook</Text>
+        <View style={styles.inputsContainer}>
+          <TextInput
+            style={styles.loginInput}
+            placeholderTextColor={'#8F8F8F'}
+            placeholder="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            autoCorrect={false}
+          />
+          <TextInput
+            style={styles.loginInput}
+            placeholderTextColor={'#8F8F8F'}
+            placeholder="Password"
+            autoCapitalize="none"
+            secureTextEntry={true}
+          />
+          <TouchableOpacity activeOpacity={0.5}>
+            <Text style={styles.forgotPasswordText}>Olvidé mi contraseña</Text>
           </TouchableOpacity>
         </View>
-      </View>
+
+        <View style={styles.loginButtonsContainer}>
+          <TouchableOpacity style={styles.logInButton} activeOpacity={0.5}>
+            <Text style={styles.loginButtonText}>Iniciar sesion</Text>
+          </TouchableOpacity>
+          <View style={styles.bottomDivisorContainer}>
+            <View style={styles.divisorLine} />
+            <Text style={styles.bottomDivisorText}>o ingresa con</Text>
+            <View style={styles.divisorLine} />
+          </View>
+          <View style={styles.socialButtonsContainer}>
+            <TouchableOpacity style={styles.socialButton} activeOpacity={0.5}>
+              <GoogleIcon height={25} width={25} />
+              <Text style={styles.socialButtonText}>Google</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.socialButton} activeOpacity={0.5}>
+              <FacebookIcon height={25} width={25} />
+              <Text style={styles.socialButtonText}>Facebook</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.spacer} />
+
+        <TouchableOpacity
+          style={styles.registerTextContainer}
+          activeOpacity={0.5}>
+          <Text style={styles.registerTextLeft}>
+            ¿Aún no tienes una cuenta?
+          </Text>
+          <Text style={styles.registerTextRight}>Únete ahora</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  screenContainer: {
+  screenContainer1: {
     flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  contentContainerStyle: {
+    flexGrow: 1,
     alignItems: 'center',
-    marginHorizontal: 20,
   },
   image: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'lightgray',
-    borderRadius: 5,
     marginTop: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 32,
     textAlign: 'center',
     marginTop: 20,
-    fontFamily: 'Poppins-Bold',
+    fontFamily: 'Poppins-Medium',
+    color: 'black',
   },
   subtitle: {
-    marginTop: 20,
     fontSize: 14,
     textAlign: 'center',
     fontFamily: 'Poppins-Regular',
+    color: '#5C5C5C',
   },
   inputsContainer: {
     width: '100%',
-    marginTop: 20,
   },
   loginInput: {
     width: '100%',
     height: 55,
     borderWidth: 0.5,
-    borderColor: '#979797',
+    borderColor: '#8F8F8F',
     borderRadius: 3,
     marginBottom: 15,
-    paddingHorizontal: 10,
+    paddingHorizontal: 20,
+    fontFamily: 'Poppins-Regular',
   },
   forgotPasswordText: {
     textAlign: 'right',
-    textDecorationLine: 'underline',
     fontFamily: 'Poppins-Regular',
+    color: '#1976D2',
+    fontSize: 15,
   },
   spacer: {
+    marginTop: 20,
     flex: 1,
   },
   loginButtonsContainer: {
@@ -107,18 +146,24 @@ const styles = StyleSheet.create({
   logInButton: {
     width: '100%',
     height: 55,
-    backgroundColor: '#6B6B6B',
+    backgroundColor: '#1E96FF',
     borderRadius: 4,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20,
   },
   loginButtonText: {
     color: 'white',
-    fontSize: 14,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontFamily: 'Poppins-SemiBold',
   },
   divisorContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 15,
+  },
+  bottomDivisorContainer: {
     width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
@@ -126,11 +171,20 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   divisorText: {
-    marginHorizontal: 10,
+    marginHorizontal: 15,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#8F8F8F',
+    fontSize: 16,
+  },
+  bottomDivisorText: {
+    marginHorizontal: 15,
+    fontFamily: 'Poppins-Medium',
+    color: '#8F8F8F',
+    fontSize: 14,
   },
   divisorLine: {
     height: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#8F8F8F',
     flex: 1,
   },
   socialButtonsContainer: {
@@ -140,16 +194,36 @@ const styles = StyleSheet.create({
   socialButton: {
     width: '48%',
     height: 55,
-    borderColor: '#B5B5B5',
+    borderColor: '#1E96FF',
     borderWidth: 1,
     borderRadius: 4,
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'row',
   },
   socialButtonText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#6B6B6B',
+    fontSize: 15,
+    color: '#1E96FF',
+    fontFamily: 'Poppins-Medium',
+    marginLeft: 10,
+  },
+  registerTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  registerTextLeft: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Regular',
+    color: '#8F8F8F',
+    fontSize: 15,
+    marginRight: 5,
+  },
+  registerTextRight: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Medium',
+    color: '#1E96FF',
+    fontSize: 15,
   },
 });
 
