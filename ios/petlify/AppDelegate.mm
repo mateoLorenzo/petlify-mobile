4214a7d2bb37ba38f11f1a5aedc381e830b8d291
mateolorenzo <mateolorenzo.dev@gmail.com>
2024-07-22 19:45:07 -0300

    removed facebook config

--- ios/petlify/AppDelegate.mm
+++ ios/petlify/AppDelegate.mm
@@ -1,17 +1,11 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h> // Asegúrate de tener esta importación
-#import <AuthenticationServices/AuthenticationServices.h>
-#import <SafariServices/SafariServices.h>
-#import <FBSDKCoreKit/FBSDKCoreKit-Swift.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [[FBSDKApplicationDelegate sharedInstance] application:application
-                       didFinishLaunchingWithOptions:launchOptions];
 
   self.moduleName = @"petlify";
   // You can add your custom initial props in the dictionary below.
@@ -19,14 +13,6 @@
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-- (BOOL)application:(UIApplication *)app
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-  return [[FBSDKApplicationDelegate sharedInstance] application:app
-                                                        openURL:url
-                                                        options:options];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
